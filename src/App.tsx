990f9b08108bc50757bebe163459b6ddd05bceaf
Rafaela Janeczko <sagethirteen@gmail.com>
2025-04-01 11:17:09 -0300

    feat: add cycles component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Menu } from './components/Menu';
 import './styles/theme.css';
 import './styles/global.css';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
 
 export default function App() {
   return (
@@ -22,14 +23,18 @@ export default function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <DefaultInput id="myInput" labelText="test" type="text" placeholder="Type something..."/>
+            <DefaultInput
+              id='myInput'
+              labelText='test'
+              type='text'
+              placeholder='Type something...'
+            />
           </div>
           <div className='formRow'>
             <span>Lorem ipsum dolor sit amet.</span>
           </div>
           <div className='formRow'>
-            <span>Cycles</span>
-            <span>+ + + + +</span>
+            <Cycles />
           </div>
           <div className='formRow'>
             <button>Send</button>
