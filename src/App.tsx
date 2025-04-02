ee094934859c8bef60d7fda903040d363e0c345b
Rafaela Janeczko <sagethirteen@gmail.com>
2025-04-02 10:37:32 -0300

    feat: create main template and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,8 @@
-import { Container } from './components/Container';
-import { Countdown } from './components/Countdown';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
 
-export default function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <Countdown />
-      </Container>
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              id='myInput'
-              labelText='test'
-              type='text'
-              placeholder='Type something...'
-            />
-          </div>
-          <div className='formRow'>
-            <span>Lorem ipsum dolor sit amet.</span>
-          </div>
-          <div className='formRow'>
-            <Cycles />
-          </div>
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} isError='' />
-          </div>
-        </form>
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+export function App() {
+  return <Home />;
 }
---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,29 @@
+import { PlayCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { DefaultInput } from '../DefaultInput';
+import styles from './styles.module.css';
+
+export function MainForm() {
+  return (
+    <form className={styles.form} action=''>
+      <div className={styles.formRow}>
+        <DefaultInput
+          id='myInput'
+          labelText='test'
+          type='text'
+          placeholder='Type something...'
+        />
+      </div>
+      <div className={styles.formRow}>
+        <span>Lorem ipsum dolor sit amet.</span>
+      </div>
+      <div className={styles.formRow}>
+        <Cycles />
+      </div>
+      <div className={styles.formRow}>
+        <DefaultButton icon={<PlayCircleIcon />} isError='' />
+      </div>
+    </form>
+  );
+}
